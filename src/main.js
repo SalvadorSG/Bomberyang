3ff5e0d862790974bfc0a5ddee21ad34957ea8c7
Salvador <salvadorsimongandia17@gmail.com>
2021-12-28 19:46:48 +0100

    arreglado parcel

--- src/main.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    var canvas = document.getElementById('canvas');
-    console.log(canvas)
-	var ctx = canvas.getContext("2d");
-}
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+window.onload = () => {
+	var canvas = document.getElementById("canvas") as HTMLCanvasElement;
+	var ctx = canvas.getContext("2d") as CanvasRenderingContext2D
+}
